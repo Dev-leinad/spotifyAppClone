5214fbf7e6edc310d6e80452af54f569b4af8808
Dev-leinad <ochidaniel075@gmail.com>
2023-03-10 11:50:57 +0100

    Added new styling

--- src/Components/RightSidebarComponent/Rightside.jsx
+++ src/Components/RightSidebarComponent/Rightside.jsx
@@ -1,11 +1,101 @@
 import React from 'react'
 import './rightside.css'
+import {BiDotsHorizontalRounded} from 'react-icons/bi' 
 
 const Rightside = () => {
   return (
     <>
 <div className="rightside">
-<p>This is RightSide</p>
+  <div className="topcont">
+    <h2>Shortcut</h2>
+    <div className="side-boxes">
+      <div className="side-box-cont">
+        <div className="emoji">
+          ❄️
+        </div>
+        <p>Chill Hits</p>
+      </div>
+      <div className="side-box-cont">
+        <div className="emoji">
+
+          🌟
+        </div>
+        <p>Hop</p>
+      </div>
+      <div className="side-box-cont">
+        <div className="emoji">
+          🎸
+        </div>
+        <p>Accoustic</p>
+      </div>
+      <div className="side-box-cont">
+        <div className="emoji">
+          🎵
+        </div>
+        <p>Indie Pop</p>
+      </div>
+      <div className="side-box-cont">
+        <div className="emoji">
+         🎹 
+        </div>
+        <p>Piano Blue</p>
+      </div>
+      <div className="side-box-cont">
+        <div className="emoji">
+          🎺
+        </div>
+        <p>Jazz</p>
+      </div>
+    </div>
+
+  </div>
+
+  <div className="fav-artist-container">
+    <h3>Fav Artist</h3>
+
+    <div className="fav-cont">
+      <div className="fav-right-col">
+        <div className="prof">
+          <img src="https://images.pexels.com/photos/15764531/pexels-photo-15764531.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+        </div>
+        <div className="fav-bio">
+          <h4>Taylor Smith</h4>
+          <p>189 songs in library</p>
+        </div>
+      </div>
+      <div className="fav-left-col">
+        <BiDotsHorizontalRounded/>
+      </div>
+    </div>
+    <div className="fav-cont">
+      <div className="fav-right-col">
+        <div className="prof">
+          <img src="https://images.pexels.com/photos/15764531/pexels-photo-15764531.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+        </div>
+        <div className="fav-bio">
+          <h4>Taylor Smith</h4>
+          <p>189 songs in library</p>
+        </div>
+      </div>
+      <div className="fav-left-col">
+        <BiDotsHorizontalRounded/>
+      </div>
+    </div>
+    <div className="fav-cont">
+      <div className="fav-right-col">
+        <div className="prof">
+          <img src="https://images.pexels.com/photos/15764531/pexels-photo-15764531.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+        </div>
+        <div className="fav-bio">
+          <h4>Taylor Smith</h4>
+          <p>189 songs in library</p>
+        </div>
+      </div>
+      <div className="fav-left-col">
+        <BiDotsHorizontalRounded/>
+      </div>
+    </div>
+  </div>
 </div>
     
     </>
--- src/Components/Widgets/Widget.jsx
+++ src/Components/Widgets/Widget.jsx
@@ -3,22 +3,39 @@ import {AiFillHome} from 'react-icons/ai'
 
 const Widget = () => {
   return (
-    <>
+    <> 
     <div className="widgets">
-    <div>
-      <AiFillHome />
-      <p> Home</p>
+    <div className='box active'>
+      <AiFillHome className='bx-icon' />
+      <h3> Home</h3>
     </div>
-    <div>
-      <AiFillHome />
-      <p> Trends</p>
+    <div className='box'>
+      <AiFillHome className='bx-icon' />
+      <h3> Trend</h3>
     </div>
-    <div>
-      <AiFillHome />
-      <p> Feed</p>
+    <div className='box'>
+      <AiFillHome className='bx-icon' />
+      <h3> Feed</h3>
     </div>
     </div>
 
+    {/* Discover section  */}
+
+    <div className="widgets">
+      <h3>Discover</h3>
+    <div className='box active'>
+      <AiFillHome className='bx-icon' />
+      <h3> Home</h3>
+    </div>
+    <div className='box'>
+      <AiFillHome className='bx-icon' />
+      <h3> Trend</h3>
+    </div>
+    <div className='box'>
+      <AiFillHome className='bx-icon' />
+      <h3> Feed</h3>
+    </div>
+    </div>
     </>
   )
 }
